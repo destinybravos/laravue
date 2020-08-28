2977ce159ee51dd761db444f65fde8e7bd7113a3
Destiny Brotobor <destinybravos@gmail.com>
2020-08-28 23:24:49 +0100

    first commit

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 justify-content-centered">
+                    <div class="card mt-5 mb-5">
+                        <div class="card-header">
+                            This is a header
+                        </div>
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eaque, voluptates consectetur porro 
+                            laborum, similique incidunt nulla quasi, deserunt enim nesciunt? Neque unde nam quisquam commodi 
+                            facere rerum incidunt consectetur.
+                        </div>
+                    </div>
+
+                    <div class="card mt-5 mb-5">
+                        <div class="card-header">
+                            This is a header
+                        </div>
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus eaque, voluptates consectetur porro 
+                            laborum, similique incidunt nulla quasi, deserunt enim nesciunt? Neque unde nam quisquam commodi 
+                            facere rerum incidunt consectetur.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="mt-5 mb-5">
+        <test-component></test-component>
+    </div>
+@endsection
