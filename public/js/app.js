874b59126726736feb48208e407b943d3d0c91c1
ferdinandtheprogrammer <68121744+ferdinandtheprogrammer@users.noreply.github.com>
2020-08-29 09:21:27 +0100

    new change

--- public/js/app.js
+++ public/js/app.js
@@ -50385,8 +50385,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\Laravue\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Laravue\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\laravue\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravue\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
