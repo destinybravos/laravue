9a74dbe2ba87e627487b32b3e91d18c4d2e22f9d
Zubisofts <zubitex40@gmail.com>
2020-08-29 08:51:13 +0100

    initial commit

---
+++ resources/js/components/ZubbyComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card">
+    <div class="card-header">Zubisofts</div>
+    <div class="card-body">
+      <h5 class="card-title">About Me</h5>
+      <p class="card-text">I'm a cool and loving guy who don't joke with codes. Like making friends especially programmers  :)</p>
+      <a href="https://github.com/zubisofts" class="btn btn-primary">Know More</a>
+    </div>
+  </div>
+</template>
